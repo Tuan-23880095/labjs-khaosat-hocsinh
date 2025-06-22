f5a8a79a65e64d0c04e3a18e832096e7bc400ea2
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2025-06-22 23:52:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from flask import Flask, send_from_directory
-from server.server import register_routes
+from flask import send_from_directory
+from server.server import create_app
 import os
 
-app = Flask(__name__, static_folder="client")
-register_routes(app)  # đăng ký các API route từ server.py
+app = create_app()
 
-# Trả về index.html khi truy cập /
 @app.route("/")
 def serve_index():
     return send_from_directory(app.static_folder, "index.html")
 
-# Phục vụ các file tĩnh (js, css, png...) từ /client
 @app.route("/<path:path>")
-def serve_static_files(path):
+def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
-# Chạy app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
