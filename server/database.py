0fce38bef5d1f166424a929237115e3616157336
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2025-06-22 20:21:06 +0700

    Update database.py

--- server/database.py
+++ server/database.py
@@ -13,26 +13,64 @@ Base = declarative_base()
 class SurveyResponse(Base):
     __tablename__ = "responses"
     id = Column(Integer, primary_key=True)
+    
+    # Thông tin cá nhân
     ho_ten = Column(String)
     lop = Column(String)
     ma_so = Column(String)
     truong = Column(String)
     khu_vuc = Column(String)
+
+    # Câu hỏi 1
     Q1 = Column(String)
+    Q1_duration = Column(Integer)         # millisec
+    Q1_timestamp = Column(DateTime)       # ISO 8601
+
+    # Câu hỏi 2
     Q2 = Column(String)
+    Q2_duration = Column(Integer)
+    Q2_timestamp = Column(DateTime)
+
+    # Câu hỏi 3
     Q3 = Column(String)
+    Q3_duration = Column(Integer)
+    Q3_timestamp = Column(DateTime)
+
+    # Câu hỏi 4
     Q4 = Column(String)
     Q4_explain = Column(Text)
+    Q4_duration = Column(Integer)
+    Q4_timestamp = Column(DateTime)
+
+    # Câu hỏi 5
     Q5 = Column(String)
     Q5_explain = Column(Text)
+    Q5_duration = Column(Integer)
+    Q5_timestamp = Column(DateTime)
+
+    # Câu hỏi 6
     Q6 = Column(String)
     Q6_explain = Column(Text)
+    Q6_duration = Column(Integer)
+    Q6_timestamp = Column(DateTime)
+
+    # Câu hỏi 7
     Q7 = Column(String)
     Q7_explain = Column(Text)
+    Q7_duration = Column(Integer)
+    Q7_timestamp = Column(DateTime)
+
+    # Câu hỏi 8
     Q8 = Column(String)
     Q8_explain = Column(Text)
+    Q8_duration = Column(Integer)
+    Q8_timestamp = Column(DateTime)
+
+    # Ý kiến cuối
     ykien = Column(Text)
-    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+    # Thời điểm nộp toàn bộ khảo sát
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
 Base.metadata.create_all(engine)
 
